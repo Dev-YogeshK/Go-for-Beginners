47c723d1ec09a18b47a0abf58e5a7a1ca65b4db2
Dev-YogeshK <100025798+Dev-YogeshK@users.noreply.github.com>
2022-02-19 17:20:45 +0530

    Variables and Datatype Declaration

---
+++ first.go
@@ -0,0 +1,29 @@
+// Author : Yogesh Kale
+package main //package main
+
+//imports
+import (
+	"fmt"
+	"log"
+)
+
+// main function
+func main() {
+	fmt.Println("Hello")
+
+	//vairable declaration
+	var iamnumber int
+	var iamstring string
+	var iamfloat float64
+
+	//Assigning Value to Variables
+	iamnumber = 8
+	iamstring = "Yogesh"
+	iamfloat = 34.54
+
+	// Displaying the value Assigned to Variable
+	fmt.Println(iamnumber)
+	log.Println(iamstring)
+	fmt.Println(iamfloat)
+
+}
